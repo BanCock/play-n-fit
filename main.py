4f2b9747de914315f78aef31d963050f1193795d
BanCock <maxfactor365@gmail.com>
2024-04-15 16:56:17 +0300

    Просто затычка чтобы создать проект

--- main.py
+++ main.py
@@ -1,50 +1,39 @@
 from kivy.app import App
-from kivy.uix.widget import Widget
-from kivy.graphics import Color, Line
-from PIL import Image
-
-class PaintWidget(Widget):
-    def on_touch_down(self, touch):
-        with self.canvas:
-            Color(1, 1, 1)
-            touch.ud['line'] = Line(points=(touch.x, touch.y))
+from kivy.uix.button import Button
+from kivy.uix.boxlayout import BoxLayout
 
 
-    def on_touch_up(self, touch):
-        touch.ud['line'].points += [touch.x, touch.y]
+class MainMenu(BoxLayout):
+    def __init__(self, **kwargs):
+        super(MainMenu, self).__init__(**kwargs)
+        self.orientation = 'vertical'
+        play_button = Button(text='Играть')
+        settings_button = Button(text='Настройки')
+        exit_button = Button(text='Выход')
 
-class PaintApp(App):
+        play_button.bind(on_press=self.start_game)
+        settings_button.bind(on_press=self.open_settings)
+        exit_button.bind(on_press=self.exit_game)
+
+        self.add_widget(play_button)
+        self.add_widget(settings_button)
+        self.add_widget(exit_button)
+
+    def start_game(self, instance):
+        print("Начать игру")
+
+    def open_settings(self, instance):
+        print("Открыть настройки")
+
+    def exit_game(self, instance):
+        print("Выход из игры")
+        App.get_running_app().stop()
+
+
+class GameApp(App):
     def build(self):
-        return PaintWidget()
+        return MainMenu()
 
-def flood_fill(image, position, target_color, replacement_color):
-    # Получаем данные изображения
-    data = image.load()
-    width, height = image.size
-    target_color = data[position]
-    to_fill = set([position])
-
-    while to_fill:
-        x, y = to_fill.pop()
-
-        if data[x, y] != target_color or data[x, y] == replacement_color:
-            continue
-
-        data[x, y] = replacement_color
-        if x > 0:
-            to_fill.add((x - 1, y))
-        if x < width - 1:
-            to_fill.add((x + 1, y))
-        if y > 0:
-            to_fill.add((x, y - 1))
-        if y < height - 1:
-            to_fill.add((x, y + 1))
 
 if __name__ == '__main__':
-    PaintApp().run()
-
-# Загрузите изображение и используйте функцию flood_fill
-image = Image.open('pressed.jpg')
-# Замените (0, 0, 0, 0) на цвет, который вы хотите сделать прозрачным
-flood_fill(image, (10, 10), (255, 0, 0, 0), (0, 0, 0, 0))
-image.save('result_image.png')
+    GameApp().run()
