379ad64ec814987f5c0b14125b1efe3d2830636c
BanCock <maxfactor365@gmail.com>
2024-04-15 16:44:52 +0300

    ааа

--- main.py
+++ main.py
@@ -1,16 +1,50 @@
-# This is a sample Python script.
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.graphics import Color, Line
+from PIL import Image
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+class PaintWidget(Widget):
+    def on_touch_down(self, touch):
+        with self.canvas:
+            Color(1, 1, 1)
+            touch.ud['line'] = Line(points=(touch.x, touch.y))
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    def on_touch_up(self, touch):
+        touch.ud['line'].points += [touch.x, touch.y]
 
+class PaintApp(App):
+    def build(self):
+        return PaintWidget()
 
-# Press the green button in the gutter to run the script.
+def flood_fill(image, position, target_color, replacement_color):
+    # Получаем данные изображения
+    data = image.load()
+    width, height = image.size
+    target_color = data[position]
+    to_fill = set([position])
+
+    while to_fill:
+        x, y = to_fill.pop()
+
+        if data[x, y] != target_color or data[x, y] == replacement_color:
+            continue
+
+        data[x, y] = replacement_color
+        if x > 0:
+            to_fill.add((x - 1, y))
+        if x < width - 1:
+            to_fill.add((x + 1, y))
+        if y > 0:
+            to_fill.add((x, y - 1))
+        if y < height - 1:
+            to_fill.add((x, y + 1))
+
 if __name__ == '__main__':
-    print_hi('ввввв')
+    PaintApp().run()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# Загрузите изображение и используйте функцию flood_fill
+image = Image.open('pressed.jpg')
+# Замените (0, 0, 0, 0) на цвет, который вы хотите сделать прозрачным
+flood_fill(image, (10, 10), (255, 0, 0, 0), (0, 0, 0, 0))
+image.save('result_image.png')
